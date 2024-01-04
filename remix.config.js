37034abf167625fdfea688a938b0d76943f97549
timzolleis <tim@zolleis.net>
2024-01-04 01:16:46 +0100

    init(app): setup application

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+/** @type {import('@remix-pwa/dev').WorkerConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+  serverDependenciesToBundle: ["@remix-pwa/sw"]
+};
